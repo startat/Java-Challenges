46d7fcf97847aabde1cbc070eceb94b6fbe81525
Chensu <65627161+startat@users.noreply.github.com>
2021-03-24 11:42:31 -0400

    Update CountingBirthdayCakeCandles.java

--- CountingBirthdayCakeCandles.java
+++ CountingBirthdayCakeCandles.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
+public class CountingBirthdayCakeCandles {
 
     // Complete the birthdayCakeCandles function below.
     static int birthdayCakeCandles(int[] ar) {
